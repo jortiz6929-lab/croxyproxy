4bfe81615cc013e2c5e92d2591a9633bdb9d93bb
jortiz6929-lab <jortiz6929@gmail.com>
2026-04-21 16:44:36 -0400

    Update uv.config.js

--- uv.config.js
+++ uv.config.js
@@ -3,7 +3,9 @@ self.__uv$config = {
     bare: [
         'https://bare.benro.dev/',
         'https://uv.student-portal.workers.dev/bare/',
-        'https://tomp.app/'
+        'https://tomp.app/',
+        'https://bare.fly.dev/',
+        'https://uv.radon.games/bare/'
     ],
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
