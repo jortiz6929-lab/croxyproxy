7193ce57cf882ed71205ba7b46779848910d89b0
jortiz6929-lab <jortiz6929@gmail.com>
2026-04-21 16:35:49 -0400

    Update uv.config.js

--- uv.config.js
+++ uv.config.js
@@ -1,9 +1,14 @@
+/* Ultraviolet Configuration 
+   This file tells the browser how to handle the proxy requests.
+*/
+
 self.__uv$config = {
     prefix: '/service/',
     bare: [
         'https://bare.benro.dev/',
         'https://uv.student-portal.workers.dev/bare/',
-        'https://tomp.app/'
+        'https://tomp.app/',
+        'https://bare.fly.dev/'
     ],
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
